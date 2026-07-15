cce94ae52e90a366dbb6a41a8b575843ce57190d
deepthireddy31 <reddydeepthi484@gmail.com>
2026-07-15 21:31:42 +0530

    Add files via upload

---
+++ python practise/prob.py
@@ -0,0 +1,8 @@
+#coding challenge/problem
+english=int(input("Enter english marks:"))
+maths=int(input("Enter maths marks:"))
+science=int(input("Enter science marks:"))
+total=english+maths+science
+print("Total",total)
+avg=(english+maths+science)/3
+print(f"Average={avg}")
---
+++ python practise/variables.py
@@ -0,0 +1,10 @@
+#creating variables of college details
+college_name="INDU"
+branch="data science"
+std_name="Deepthi"
+std_id="24D41A6788"
+year="3rd year"
+print("college name is:",college_name)
+print(branch)
+print(std_name)
+print(std_id)
