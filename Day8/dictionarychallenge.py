b63cc0fb6a81b4d65a83ced80faf302185124e30
deepthireddy31 <reddydeepthi484@gmail.com>
2026-07-27 16:18:44 +0200

    Add files via upload

---
+++ Day8/dictionarychallenge.py
@@ -0,0 +1,39 @@
+#dictionary coding challenge:
+student_report_system={
+    "name":"pritam",
+    "year":"3rd",
+    "branch":"data science",
+    "college":"sri indu college"
+}
+print("student details:",student_report_system)
+for key,values in student_report_system.items():
+    print(key,values)
+Employee_database={
+    "emp_name":"pedro",
+    "emp_salary":"50,000",
+    "emp_address":"hyderabad",
+    "emp_role":"data scientist"
+}
+print("employee datbase:",Employee_database)
+for key,values in Employee_database.items():
+    print(key,values)
+Library_management={
+    "std_name":"rahul",
+    "std_id":"24D41A6792",
+    "no.of.books taken":4,
+    "library_incharge":"padmaja",
+    "library_incharge_id":290
+}
+print("library management:",Library_management)
+for key,values in Library_management.items():
+    print(key,values)
+Phone_book={
+"contact_name1":"madhu",
+"contact_phno1":9345678231,
+"contact_name2":"john",
+"contact_phno2":6303478231,
+"total_contacts":2,
+}
+print("phone book:",Phone_book)
+for key,values in Phone_book.items():
+    print(key,values)
