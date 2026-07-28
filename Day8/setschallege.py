6b8a2f0cf20e8265266d9c8c7c9d83253600be4f
deepthireddy31 <reddydeepthi484@gmail.com>
2026-07-28 18:35:21 +0530

    Implement set challenge for common student IDs
    Add a coding challenge to find common student IDs between two lists.

--- Day8/setschallege.py
+++ Day8/setschallege.py
@@ -1,1 +1,11 @@
- 
+#set coding challege:
+std1_id=[22,31,69,45,78,69,31]
+new_std1_id=set(std1_id)
+std2_id=[56,31,69,45,56,34,22]
+new_std2_id=set(std2_id)
+for item in new_std1_id: 
+   if item in new_std2_id:
+     print(item) 
+#print(new_std1_id & new_std2_id) 
+#common=new_std1_id.intersection(new_std2_id) 
+#print("common",common) 
