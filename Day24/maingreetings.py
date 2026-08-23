a9854385d81017a11ed2f6fbdadaf622a7c2a658
DEEPTHI REDDY KOMPALLY <reddydeepthi484@gmail.com>
2026-08-23 22:36:56 +0530

    Add main greetings script to use greeting module

---
+++ Day24/maingreetings.py
@@ -0,0 +1,5 @@
+#importing the module which has created in previous(greetingmodule.py)
+import greetingsmodule
+
+print(greetingsmodule.greet("madhuri","hyd"))
+print(greetingsmodule.greet("rahul","chennai"))
