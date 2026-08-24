ca003b2cdd66b0c292f94fb34f4fa320516b8db5
DEEPTHI REDDY KOMPALLY <reddydeepthi484@gmail.com>
2026-08-24 21:54:39 +0530

    Update square root function and add pi output
    Refactor mathmodule.py to improve function and output.

--- Day24/mathmodule.py
+++ Day24/mathmodule.py
@@ -1,6 +1,6 @@
 #math(bulid in module) is taking using allias 
-import math as m
-def pi(a):
-    print(m.pi(34)) 
+import math as m 
 def squartroot(a):
-    print(m.sqrt(5))
+    print(m.sqrt(a))
+    print(m.pi)
+squartroot(49)
