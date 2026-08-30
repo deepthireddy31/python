9af031d453fe46380a2c76740afcfb34dec8efda
DEEPTHI REDDY KOMPALLY <reddydeepthi484@gmail.com>
2026-08-30 18:56:02 +0530

    Update journal entries in POTD.py
    Removed the greet function and added daily journal entries to a text file.

--- Day26/POTD.py
+++ Day26/POTD.py
@@ -1,7 +1,6 @@
-
-def greet():
-    print("Hello!")
-
-say_hello = greet
-
-say_hello()
+#POTD:DAILY JOURNAL
+with open("day26potd_journal.txt","w")as file:
+    file.write("\nToday,i'm feeling bored ")
+    file.write("\nI had my lunch ")
+with open("day26potd_journal.txt","a")as file:
+    file.write("\nI learned about file handlings")
